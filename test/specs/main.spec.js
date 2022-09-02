0e947e201b402c9a13d50eab8b37c946bcce88e4
javierbrea <javier.brea@gmail.com>
2022-09-02 07:53:18 +0200

    test: Add test about mounting config file

--- test/specs/main.spec.js
+++ test/specs/main.spec.js
@@ -69,7 +69,7 @@ describe("main image", () => {
           "docker",
           "run",
           "-v",
-          `${fixturesPath("mocks")}:/input/mocks`,
+          `${fixturesPath("project/mocks")}:/input/mocks`,
           "-p",
           "3100:3100",
           "-p",
@@ -96,6 +96,76 @@ describe("main image", () => {
       expect(dockerProcess.logs.all).toEqual(
         expect.stringContaining("Configuration file was not found. A scaffold was created")
       );
+    });
+
+    it("should have available the books route", async () => {
+      const response = await doFetch("/api/books");
+      expect(response.body).toEqual([
+        {
+          id: 1,
+          title: "1984",
+        },
+        {
+          id: 2,
+          title: "Brave New World",
+        },
+      ]);
+    });
+
+    it("should have available the book route", async () => {
+      const response = await doFetch("/api/books/1");
+      expect(response.body).toEqual({
+        id: 1,
+        title: "1984",
+      });
+    });
+  });
+
+  describe("When mocks and config are mounted", () => {
+    beforeAll(async () => {
+      await cleanDocker();
+      dockerProcess = new Spawner([
+        "docker",
+        "run",
+        "-v",
+        `${fixturesPath("project")}:/input`,
+        "-p",
+        "3100:3100",
+        "-p",
+        "3110:3110",
+        "mocks-server:main",
+      ]);
+      await waitForServerUrl("/api/books");
+    }, DOCKER_TIMEOUT_LIMIT);
+
+    afterAll(async () => {
+      await dockerProcess.kill();
+      await wait(DOCKER_COURTESY_TIME);
+    }, DOCKER_TIMEOUT_LIMIT);
+
+    it("should have not created the scaffold", async () => {
+      expect(dockerProcess.logs.all).toEqual(
+        expect.not.stringContaining("Mocks Server folder was not found. A scaffold was created")
+      );
+    });
+
+    it("should have not created the config file", async () => {
+      expect(dockerProcess.logs.all).toEqual(
+        expect.not.stringContaining("Configuration file was not found. A scaffold was created")
+      );
+    });
+
+    it("should have not logged silly logs", async () => {
+      expect(dockerProcess.logs.all).toEqual(expect.not.stringContaining("[silly]"));
+    });
+
+    it("should have logged debug logs", async () => {
+      expect(dockerProcess.logs.all).toEqual(expect.stringContaining("[debug]"));
+    });
+
+    it("should have debug log level in the adminApi config route", async () => {
+      const response = await doFetch("/api/config", { port: 3110 });
+      expect(response.body.log).toEqual("debug");
     });
 
     it("should have available the books route", async () => {
